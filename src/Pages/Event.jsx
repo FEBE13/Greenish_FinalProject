815fc1fceea1c61a538b3a8ae30f6b0eedeed538
DaffaRadh <daffasven@gmail.com>
2022-11-24 20:01:16 +0700

    banner event

--- src/Pages/Event.jsx
+++ src/Pages/Event.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import BannerEvent from '../Components/BannerEvent'
 import Navi from '../Components/Navi'
 
 function Event() {
   return (
     <div>
         <Navi/>
-        Event
+        <BannerEvent/>
     </div>
   )
 }
