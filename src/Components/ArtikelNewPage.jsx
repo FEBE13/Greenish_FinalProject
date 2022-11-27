f5f94e0fec080113983dc580b90d7dcc5724b569
hadayasyahla <hadayasyahla@gmail.com>
2022-11-27 23:15:09 +0700

    newspage

---
+++ src/Components/ArtikelNewPage.jsx
@@ -0,0 +1,37 @@
+function ArtikelNewsPage () {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col">
+
+                    <h1>Dampak negatif dari pembakaran hutan</h1>
+                    <img src="https://res.cloudinary.com/ddhkwq4zk/image/upload/v1669308067/Rectangle_1321_d2s2cc.png" alt="" />
+
+                </div>
+                <div className="col">
+                <div className="card mb-3" style={{maxWidth: "540px"}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src="https://res.cloudinary.com/ddhkwq4zk/image/upload/v1669561508/image_6_jknjxd.png" className="img-fluid rounded-start" alt="..."/>
+    </div>
+    
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Yang hilang dari pidato kenegaraan jokowi</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
+export default ArtikelNewsPage
