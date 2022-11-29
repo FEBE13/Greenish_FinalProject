aff5ffb0bea32721c48c50058c4072932324079d
DaffaRadh <daffasven@gmail.com>
2022-11-29 11:23:25 +0700

    list event with axios and reducer

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -1,19 +1,69 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../Redux/Action/EventAction";
+import { useNavigate } from "react-router-dom";
+
 
 function CardComponent() {
+  const nav = useNavigate()
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.Event);
+  const item = data.events;
+  // item.map((item)=>{
+  //   const date = new Date (item.date)
+  //   date.getYear()
+  //   console.log(date)
+  // })
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
   return (
-    <div style={{width:'100%', padding:'20px',display:'flex',justifyContent:'center',flexWrap:'wrap'}}>
-        <Card className='carde' style={{margin:'10px',maxHeight:'370px',maxWidth:'300px',border:'none',cursor:'pointer'}}>
-        <Card.Img style={{objectFit:'cover'}} variant="top" src="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/events/2022/10/24/d8b09805-73f7-4695-95d4-21546cb36b18-1666585095578-48fd0f85473d167f779c6f6ac9b9d325.jpeg"/>
-        <Card.Body style={{padding:'15px',lineHeight:'20px'}}>
-            <p style={{fontWeight:'600',fontSize:'18px'}}>EVERGLOW SOUTHEAST ASIA TOUR in Jakarta</p>
-            <p style={{fontWeight:'300',fontSize:'15px'}}>Jakata Selatan</p>
-            <p style={{fontWeight:'300',fontSize:'15px'}}>21 Des 2022</p>
-        </Card.Body>
-        </Card>
+    <div
+      style={{
+        width: "100%",
+        padding: "20px",
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      {item.map((item,index) => {
+        return (
+          <Card
+            onClick={()=>{nav(`/event/${item._id}`)}}
+            key={item._id}
+            className="carde"
+            style={{
+              margin: "10px",
+              maxHeight: "370px",
+              maxWidth: "300px",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            <Card.Img
+              style={{ objectFit: "cover",height:'200px'}}
+              variant="top"
+              src={item.poster}
+            />
+            <Card.Body style={{ padding: "15px", lineHeight: "20px" }}>
+              <p style={{ fontWeight: "600", fontSize: "18px" }}>
+                {item.name}
+              </p>
+              <p style={{ fontWeight: "300", fontSize: "15px" }}>
+                {item.place}
+              </p>
+              <p style={{ fontWeight: "300", fontSize: "15px" }}>21 Des 2022</p>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default CardComponent
+export default CardComponent;
